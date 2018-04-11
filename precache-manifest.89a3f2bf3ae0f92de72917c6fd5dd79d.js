3e956b4de075d6c87cbe895b54d63d956ace6777
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-11 07:42:59 +0000

    Deploy app4site/ppldo to github.com/app4site/ppldo.git:gh-pages

---
+++ precache-manifest.89a3f2bf3ae0f92de72917c6fd5dd79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "45c5a146d64d97e76e6b",
+    "url": "/ppldo/js/vendor.45c5a146.js"
+  },
+  {
+    "revision": "3ef0a44d241d189afbb1",
+    "url": "/ppldo/js/manifest.3ef0a44d.js"
+  },
+  {
+    "revision": "1b1dc99c6aa38dccbab0",
+    "url": "/ppldo/js/app.1b1dc99c.js"
+  },
+  {
+    "revision": "ca434eaa5cef360c79c794125d8f76cf",
+    "url": "/ppldo/index.html"
+  },
+  {
+    "revision": "1b1dc99c6aa38dccbab0",
+    "url": "/ppldo/css/app.cd4af45b.css"
+  }
+];
